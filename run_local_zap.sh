425705071bc783ba8bfa97c2af4e7bf5c6d88f64
harsha <harsha.reddy@digital.hmrc.gov.uk>
2019-10-10 10:48:06 +0100

    Amend ZAP config to use latest zap version; Update context path in zap config

--- run_local_zap.sh
+++ run_local_zap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbt -Dbrowser=zap-local-chrome -Denvironment=local "testOnly uk.gov.hmrc.cucumber.runner.RunZap"
+sbt -Dbrowser=zap-local-chrome -Dzap.proxy=true -Denvironment=local "testOnly uk.gov.hmrc.cucumber.runner.RunZap"
 sbt "testOnly uk.gov.hmrc.cucumber.runner.ZapSpec"
